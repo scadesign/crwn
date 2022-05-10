e09174c7a1ee55c9ec84688128a46538e12bc66d
Stephen Arscott <stephenarscott@Stephens-iMac.local>
2022-05-10 14:47:12 +0100

    set up sign up, sign in component and crerated an input component and button component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import { Routes, Route } from 'react-router-dom';
 
-import Navigation from './routes/navigation/navigation.component';
 import Home from './routes/home/home.component';
-import SignIn from './routes/sign-in/sign-in-component';
+import Navigation from './routes/navigation/navigation.component';
+import Authentication from './routes/authentication/authentication.component';
 
 const Shop = () => {
-  return (
-    <div>
-      <h2>This is the shop</h2>
-    </div>
-  );
+  return <h1>I am the shop page</h1>;
 };
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Navigation />}>
+      <Route path='/' element={<Navigation />}>
         <Route index element={<Home />} />
-        <Route path="shop" element={<Shop />} />
-        <Route path="sign-in" element={<SignIn />} />
+        <Route path='shop' element={<Shop />} />
+        <Route path='auth' element={<Authentication />} />
       </Route>
     </Routes>
   );
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,21 +1,23 @@
 import { Fragment } from 'react';
 import { Outlet, Link } from 'react-router-dom';
+
 import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
+
 import './navigation.styles.scss';
 
 const Navigation = () => {
   return (
     <Fragment>
-      <div className="navigation">
-        <Link className="logo-container" to="/">
-          <CrwnLogo className="logo" />
+      <div className='navigation'>
+        <Link className='logo-container' to='/'>
+          <CrwnLogo className='logo' />
         </Link>
-        <div className="nav-links-container">
-          <Link className="nav-link" to="shop">
-            Shop
+        <div className='nav-links-container'>
+          <Link className='nav-link' to='/shop'>
+            SHOP
           </Link>
-          <Link className="nav-link" to="sign-in">
-            Sign In
+          <Link className='nav-link' to='/auth'>
+            SIGN IN
           </Link>
         </div>
       </div>
