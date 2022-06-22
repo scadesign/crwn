3760c6fd66dc461b7bfb104813eba2bbad7d34e2
Stephen Arscott <stephenarscott@Stephens-iMac.local>
2022-06-22 14:34:39 +0100

    created checkout page and added cart functionality

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Home from './routes/home/home.component';
 import Navigation from './routes/navigation/navigation.component';
 import Authentication from './routes/authentication/authentication.component';
 import Shop from './routes/shop/shop.component';
+import Checkout from './routes/checkout/checkout.component';
 
 const App = () => {
   const { currentUser } = useContext(UserContext);
@@ -16,6 +17,7 @@ const App = () => {
         <Route index element={<Home />} />
         <Route path="shop" element={<Shop />} />
         <Route path="auth" element={currentUser ? <Navigate to="/" replace /> : <Authentication />} />
+        <Route path="checkout" element={<Checkout />} />
       </Route>
     </Routes>
   );
